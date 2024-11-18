86c4034347987349032791c7c6997cb1b27af11c
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-17 21:51:30 -0500

    Metoso de string

---
+++ 04_clase-string-y-sus-metodos.js
@@ -0,0 +1,21 @@
+// Tipo de dato: string
+
+let string1 = 'Hola, mundo'
+let string2 = "JavaScript es genial"
+
+//Concatecacion de strings
+let string3 = `${string1} feliz :)`
+let string4 = string1 + ' ' + string2
+
+//console.log(string1)
+//console.log(string2)
+//console.log(string3)
+//console.log(string4)
+
+//Metodos de los strings LOS CUALES NOS PERMITE HACER OPERACIONES CON LOS STRINGS
+// DEPENDIENDO DE COMO LO QUERAMOS TRANSFORMAR SU VALOR O SU FORMA
+let frase = 'JavaScript es Extremadamente Genial'
+console.log(frase.length)
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+console.log(frase.substring(0, 10))
