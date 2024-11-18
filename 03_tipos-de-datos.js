45870af72783f8cc0dbeb2d622f89404468a0a14
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-17 21:51:15 -0500

    Tipos de datos

---
+++ 03_tipos-de-datos.js
@@ -0,0 +1,60 @@
+// En javascript exixten 10 tipos de datos de lso cuales.}
+
+// 7 de ellos son primitivos y 3 son objetos con mas complejidad de uso.
+
+//LOS DATOS PRIMITIVOS SON: 
+/**
+    * string
+    * number
+    * boolean 
+    * null
+    * undefined
+    * symbol
+    * bigint
+ */
+
+// LOS DATOS DE TIPO OBJETO SON:
+/**
+    * Object
+    * Function
+    * Array
+ */
+
+//EJEMPLOS DE CADA UNO DE ELLOS:
+
+// PRIMITIVOS
+
+// string
+let nombre = 'Tere'
+
+// number
+let edad = 25
+
+// boolean
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+// undefined 
+let noDefinido = undefined
+
+// symbol
+let simboloUnico = Symbol('único')
+
+// bigint
+let numeroGrande = 2n
+
+// COMPLEJOS
+
+// object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+// array
+let frutas = ['manzana', 'banano', 'uvas']
+
+// function
+function saludar() { }
