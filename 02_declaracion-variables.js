d6707418f1b975fe2330197b84f865169ce89cf3
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-17 21:51:05 -0500

    Declaraciojn de variables

---
+++ 02_declaracion-variables.js
@@ -0,0 +1,29 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+
+// Lo  No permitido en las declaraciones es las palabras de tipo hungaro
+// Siempre debe de ser un nombre que tenga sentido no como usar
+//Esto no es permitido aunque funciona pero no sabremos 
+// a que se refiere su nombre ya quer no es descriptivo
+
+
+let cda = 'Woody'
+console.log(cda) // Esto  se imprimira pero no es recomendable
+
+
+// Lo que si podemos hacer al nombrar nuestras variables
+// Aunque sus nombres sean largos pero que sean descriptivos
+
+// lO QUE SE PUEDE NOMBRAR COMO PARA QUE SE PUDA USAR ES LO SIGUINETE ESTAS NOMENCLATURAS
+// YA ESTAN DEFINIDAS Y SDON UN ESTANDAR POR EJEMPLO
+
+// Esto se poidria usar pero para ya estandares como el FBI , CIA , y demas entidades
+
+// Veamos un ejempl más practico y sencillo de entender  con las url´
+
+let urlDeLaPagina = 'https://www.google.com'
+let userId = '123456'
+
+console.log(urlDeLaPagina)
+console.log(userId)
